e3b3f708c26768a6ce4f005dee4f019d11308b42
Saifudeen369 <saifudheen232003@gmail.com>
2025-03-13 10:33:15 +0530

    Update array.sh

--- MAR/array.sh
+++ MAR/array.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
 
-# Input the number of elements
 echo "Enter the number of elements in the array:"
-read n  # This takes the number of elements the user wants to enter
+read n  
 
-# Initialize an empty array
+
 array=()
 
-# Input elements using a loop
+
 echo "Enter $n elements:"
 for ((i=0; i<n; i++)); do
-  read element  # Read user input into 'element'
-  array+=("$element")  # Add the input element to the array
+  read element  
+  array+=("$element") 
 done
 
-# Display the entire array
+
 echo "The elements in the array are: ${array[@]}"
 
