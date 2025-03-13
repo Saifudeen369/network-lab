908493c5f7ac751e8c2bb3f3c38a42afd8d55c22
Saifudeen369 <saifudheen232003@gmail.com>
2025-03-13 10:56:32 +0530

    Update expression.sh

--- MAR/week 2/expression.sh
+++ MAR/week 2/expression.sh
@@ -1,1 +1,12 @@
+echo "enter a"
+read a
+echo "enter b"
+read b
+echo "enter c"
+read c
+echo "enter d"
+read d
+val=`expr $a \* 20 - $b \* 2 + $c / $d`
+echo "value after evaluating expression is $val"
+
 
